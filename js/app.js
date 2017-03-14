d691d04659b472e6270d31e0c359cf3efccd5f90
agnieszka <agnieszka.rydz@gmail.com>
2017-03-14 18:39:01 +0100

    navbar fixed

--- js/app.js
+++ js/app.js
@@ -1,18 +1,18 @@
 $(document).ready(function(){
   $('.slogan').fadeIn(1200);
 
-		//scrolling
-	var menuElem = $(".list-nav").find("a");
-			menuElem.on("click", function(e) {
-				var element = $(this).attr("href");
-				var elementOffset = $(element).offset();
-			$("body", "html").animate({scrollTop: elementOffset.top}, 1500);
+    //scrolling
+  var menuElem = $(".list-nav").find("a");
+      menuElem.on("click", function(e) {
+        var element = $(this).attr("href");
+        var elementOffset = $(element).offset();
+      $("body", "html").animate({scrollTop: elementOffset.top}, 1500);
 
 });
 
-			  // offer-bear animation:	
-   $(".offer-bear-animation").on("mouseenter", function(){   	
-   		//$(this).find("p").animate({opacity:1},1500);
+        // offer-bear animation:  
+   $(".offer-bear-animation").on("mouseenter", function(){    
+      //$(this).find("p").animate({opacity:1},1500);
         $(".offer-animate-1").delay(800).animate({opacity:1},1500)
         $(".offer-animate-2").delay(1600).animate({opacity:1},1500)
         $(".offer-animate-3").delay(3000).animate({opacity:1},1500)
